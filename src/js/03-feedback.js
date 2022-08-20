a277c4bf258ee0a786665f7343baa41970d63b2a
Ulyanna <ulyanna16@gmail.com>
2022-08-20 20:26:56 +0300

    add third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,25 +4,51 @@ const formEl = document.querySelector('.feedback-form');
 const inputEl = document.querySelector('input');
 const textAreaEl = document.querySelector('textarea');
 
-formEl.addEventListener('input', onFormInput);
-inputEl.addEventListener('input', onInputEnter);
-textAreaEl.addEventListener('input', onTextAreaInput);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateForm();
 
 function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
   event.preventDefault();
-  console.dir(event.target.value);
-  console.dir(event.target);
-  //   const formData = {
-  //     email,
-  //     message,
-  //   };
-  //   console.log(formData);
-  //   localStorage.setItem('feedback-form-state', formData);
-  //   formEl.reset();
+  const email = event.currentTarget.elements.email.value;
+  const message = event.currentTarget.elements.message.value;
+  const formDataToConsole = {
+    email,
+    message,
+  };
+  console.log(formDataToConsole);
+  formEl.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
-function onInputEnter(event) {
-  //   console.log(event.target.value);
+
+function populateForm() {
+  const savedDataset = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedDataset) {
+    inputEl.value = savedDataset.email;
+    textAreaEl.value = savedDataset.message;
+  }
+  checkFieldsInStorage();
 }
-function onTextAreaInput(event) {
-  //   console.log(event.target.value);
+
+function checkFieldsInStorage() {
+  const storageObj = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (!storageObj) {
+    return;
+  }
+  if (!storageObj.hasOwnProperty('email')) {
+    inputEl.value = '';
+  }
+  if (!storageObj.hasOwnProperty('message')) {
+    textAreaEl.value = '';
+  }
 }
